370728484eee1f46bb8146e02cec45ba0058c23e
Alexander Kravchuk <alexanderkr@playtika.com>
2021-09-07 12:30:03 +0300

    Initial commit

---
+++ ExoPlayerSample/MainActivity.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.OS;
+using AndroidX.AppCompat.App;
+using Com.Google.Android.Exoplayer2.Source.Hls.Playlist;
+
+namespace ExoPlayerSample
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            var _ = typeof(HlsMasterPlaylist);
+        }
+	}
+}
